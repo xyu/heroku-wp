a9ef82a548c1d0ec9f6c7b1bb11414a66d087f6f
Xiao Yu <me@xyu.io>
2014-06-22 21:25:46 -0400

    Test out memcache

--- public/wp-content/plugins/howdy/howdy.php
+++ public/wp-content/plugins/howdy/howdy.php
@@ -1,6 +1,20 @@
 <?php
 	phpinfo();
 
+	// try out memcache
+	Memcached::setSaslAuthData( $ENV['MEMCACHIER_USERNAME'], $ENV['MEMCACHIER_PASSWORD'] );
+
+	$m_server = parse_url( $ENV['MEMCACHIER_SERVERS'] );
+	$m = new Memcached();
+
+	var_dump(
+		$m->addServer( $m_server['host'], $m_server['port'] )
+	);
+
+	var_dump(
+		$m
+	);
+
 	var_dump( $_SERVER );
 
 	var_dump( $_ENV );
