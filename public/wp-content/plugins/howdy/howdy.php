2bca949fab3da91f8e0beb23f961aa986966e7ff
Xiao Yu <me@xyu.io>
2014-06-23 17:33:49 -0400

    show env vars

--- public/wp-content/plugins/howdy/howdy.php
+++ public/wp-content/plugins/howdy/howdy.php
@@ -1,7 +1,7 @@
 <?php
 	phpinfo();
 
-echo "REQUEST:\n";
+echo "\nREQUEST:\n";
 foreach (apache_request_headers() as $header => $value) {
     echo "$header: $value\n";
 }
@@ -9,3 +9,5 @@ echo "\nRESPONSE:\n";
 foreach (apache_response_headers() as $header => $value) {
     echo "$header: $value \n";
 }
+echo "\nENV:\n";
+print_r( $_ENV );
