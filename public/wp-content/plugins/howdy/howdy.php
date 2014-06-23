ebad6591d58dbf11158c6c0ac1aab33785f2343d
Xiao Yu <me@xyu.io>
2014-06-22 23:57:17 -0400

    test with ironcache x3

--- public/wp-content/plugins/howdy/howdy.php
+++ public/wp-content/plugins/howdy/howdy.php
@@ -5,7 +5,7 @@
 	$m = new Memcached();
 
 	$m->addServer( 'cache-aws-us-east-1.iron.io', 11211 );
-	$m->set('oauth', "{$ENV['IRON_CACHE_TOKEN']} {$ENV['IRON_CACHE_PROJECT_ID']} WP", 0);
+	$m->set('oauth', "{$_ENV['IRON_CACHE_TOKEN']} {$_ENV['IRON_CACHE_PROJECT_ID']} WP", 0);
 
 	$m->set( 'test', 'blagh', 1 );
 	var_dump( $m->get( 'test' ) );
