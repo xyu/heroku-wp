6a409c3667138a170f88a9a91ffbae15030a1a85
Xiao Yu <me@xyu.io>
2016-08-12 22:23:01 +0000

    Turn off debug mode

--- public/wp-config.php
+++ public/wp-config.php
@@ -143,7 +143,7 @@ define( 'WPLANG', '' );
  * It is strongly recommended that plugin and theme developers use WP_DEBUG
  * in their development environments.
  */
-define( 'WP_DEBUG', true );
+define( 'WP_DEBUG', false );
 
 /* That's all, stop editing! Happy blogging. */
 
