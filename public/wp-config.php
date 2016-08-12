643463c42574a3c39468c1ecb7beb7af80354f42
Xiao Yu <me@xyu.io>
2016-08-12 20:59:00 +0000

    Turn debug on

--- public/wp-config.php
+++ public/wp-config.php
@@ -143,7 +143,7 @@ define( 'WPLANG', '' );
  * It is strongly recommended that plugin and theme developers use WP_DEBUG
  * in their development environments.
  */
-define( 'WP_DEBUG', false );
+define( 'WP_DEBUG', true );
 
 /* That's all, stop editing! Happy blogging. */
 
