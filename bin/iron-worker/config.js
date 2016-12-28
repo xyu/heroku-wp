0e3fea99db7559d217f6ea12d9d04afadb44121c
Xiao Yu <me@xyu.io>
2016-12-28 06:06:49 +0000

    Move more settings into config file

--- bin/iron-worker/config.js
+++ bin/iron-worker/config.js
@@ -1,6 +1,11 @@
 var config = {
 	timeout: 750,
-	heroku_slug: '{HEROKU_SLUG}'
+	request: {
+		hostname: '{HEROKU_SLUG}.herokuapp.com',
+		port: 443,
+		path: '/wp-cron.php?doing_wp_cron',
+		method: 'GET'
+	}
 };
 
 module.exports = config;
